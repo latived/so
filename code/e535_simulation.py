50695b3912a0cf2375580cb97b7fe64ad9604362
latived <vitalwarley@gmail.com>
2019-02-27 08:42:46 -0300

    Fix last block size
    - It could exceed `mem_size`. We just crop it.
    - Or could exist a "missing block", that it's, a gap at the end. In this
    case, we just create a block with the gap's size.

--- code/e535_simulation.py
+++ code/e535_simulation.py
@@ -19,6 +19,14 @@ class Simulator:
         self.blocks = []
 
     def build_blocks(self, min_block=0.1, max_block=1.0):
+        # `min_block` is the min. size of a block given by `min_block/100 * mem_size`
+        # `max_block` is the max. size of a block given by `max_block/100 * mem_size`
+        #
+        # If total size of `blocks` exceeds `mem_size` we crop the last (don't
+        # care its size).
+        # 
+        # If total size of `blocks` is less than `mem_size` we add a new one
+        # with its size as the differente between those (don't care its size).
 
         if len(self.blocks):
             print("Non-empty memory. Try clearing with `clear_blocks` first.")
@@ -32,6 +40,13 @@ class Simulator:
             b = random.randint(lbv, hbv)
             sum_blocks += b
             self.blocks.append(b)
+
+        last = self.mem_size - sum_blocks
+
+        if last > 0:
+            self.blocks.append(last)
+        elif last < 0:
+            self.blocks[-1] += last
 
     def clear_blocks(self):
         self.blocks = []
